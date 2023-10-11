6252497c69de53c2a4cff835d5f569f8802abbf9
nosiee <denis.lusto@gmail.com>
2023-10-11 12:36:19 +0000

    we can actually make it a little easier and faster

--- 209_word_pattern.cpp
+++ 209_word_pattern.cpp
@@ -7,41 +7,32 @@
 bool word_pattern(std::string pattern, std::string s) {
     // pretty similar problem to https://leetcode.com/problems/isomorphic-strings/
 
-    // at first, split the string by space 
-    std::vector<std::string> words;
-    std::stringstream stream(s);
-    std::string word;
-    
-    while (getline(stream, word, ' ')) {
-        words.push_back(word);
-    }
-
-    // the number of characters in the pattern must equal the number of words
-    if (pattern.length() != words.size())
-        return false;
-
-
     std::unordered_map<char, std::string> m1;
     std::unordered_map<std::string, char> m2;
+    std::stringstream stream(s);
+    std::string word;
+    int i = 0;
 
-    // map each word to each character in the pattern
-    for (int i = 0, size = words.size(); i < size; i++) {
+    // split a string by space character, and map characters and words to each other
+    while (getline(stream, word, ' ')) {
         if (m1.count(pattern[i]) > 0) {
             // if we've already added such a character and the word is different, then s does not match the pattern
-            if (m1[pattern[i]] != words[i])
+            if (m1[pattern[i]] != word)
                 return false; 
         } else {
-            m1[pattern[i]] = words[i];
+            m1[pattern[i]] = word;
         }
 
-        if (m2.count(words[i]) > 0) {
+        if (m2.count(word) > 0) {
             // if we've already added such a word and the character is different, then s does not match the pattern
-            if (m2[words[i]] != pattern[i])
+            if (m2[word] != pattern[i])
                 return false;
         } else {
-            m2[words[i]] = pattern[i];
+            m2[word] = pattern[i];
         }
+
+        i++;
     }
 
-    return true;
+    return i == pattern.size();
 }
