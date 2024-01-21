301c4780a19ef7456d2950035e5ff819b3bfc05a
nosiee <denis.lusto@gmail.com>
2024-01-21 18:02:03 +0300

    add 1822

---
+++ 1822_sign_of_the_product_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int signFunc(const int n) {
+    if (n > 0) return 1;
+    if (n < 0) return -1;
+
+    return 0;
+}
+
+int arraySign(std::vector<int> &nums) {
+    int product = 1;
+
+    // we can ignore the number itself and only use its sign
+    for (const int &n: nums) {
+       if (n == 0) return 0; 
+       if (n > 0) product *= 1;
+       if (n < 0) product *= -1;
+    }
+
+    // instead, we could look at the number of negatives in nums. and if its even, the total product is positive and opposide 
+    return signFunc(product);
+}
