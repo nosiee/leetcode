e2efe3bc5ae8c22af4295b4637cb37f38207a962
nosiee <denis.lusto@gmail.com>
2024-03-03 12:38:04 +0300

    update comment

--- 1984_minimum_difference_between_highest_and_lowets_of_k_scores.cpp
+++ 1984_minimum_difference_between_highest_and_lowets_of_k_scores.cpp
@@ -13,6 +13,9 @@ int minumum_difference(std::vector<int> &nums, int k) {
     // take a window with k nums: {(9, 7), 4, 1} and find the difference which is 9 - 7 = 2 in this case
     // it's obvious that each number on the left side of the window, is always the max number in this window
     // and the right number in the window is always the min number in this window
+    // we don't need to check all possible variants of k numbers, because the array is sorted
+    // and it's obvious that the min differece will be between k neighboring numbers
+    // so the cases like: 9-1 or, 7 - 1 is redundant
 
     for (int i = 0; (i + k - 1) < nums.size(); i++) {
         if (min && (nums[i] - nums[i + k - 1] < min)) {
